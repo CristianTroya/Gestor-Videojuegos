8a4c726618335d17ab5dcdfa56771fb67ddab58f
Cristian Troya <ctromor410@g.educaand.es>
2026-02-24 09:09:15 +0100

    Añadida interactividad mediante eventos de usuario

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
 console.log("Terminal de [Tu Nombre] iniciada con éxito");
 console.warn("Acceso de desarrollador detectado");
+// 1. Seleccionamos el botón
+const miBoton = document.querySelector("#champiñon");
+
+// 2. Le pedimos que escuche el "click"
+miBoton.addEventListener("click", function () {
+    const articulos = document.querySelectorAll("article");
+
+    articulos.forEach((a) => {
+        a.style.transform = "scale(1.03)"
+    })
+});
