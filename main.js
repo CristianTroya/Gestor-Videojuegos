c7d4b930bdfacfacc65148d25fe1180ecbaafac2
Elio <edeldia384@g.educaand.es>
2026-02-26 09:23:49 +0100

    Sistema de cambio de tema dinámico implementado

--- main.js
+++ main.js
@@ -21,10 +21,19 @@ miBoton.addEventListener("click", function () {
     })
 });
 
+const btnModo = document.getElementById("btn-modo");
+const sol = document.getElementById("sol");
+const luna = document.getElementById("luna");
 
-
-
-
-
+btnModo.addEventListener("click", function () {
+    document.body.classList.toggle("darkmode");
+    if (sol.hidden === true) {
+        sol.hidden = false;
+        luna.hidden = true;
+    } else {
+        sol.hidden = true;
+        luna.hidden = false;
+    }
+})
 
 
