ffd1c93a05e534b8886cd428a1a581f915406189
Elio <edeldia384@g.educaand.es>
2026-04-21 09:12:22 +0200

    Creado de tarjetas dinamicas

--- Flask/app.py
+++ Flask/app.py
@@ -1,4 +1,4 @@
-# Importamos la clase Flask desde el paquete instalado
+import sqlite3
 from flask import Flask, render_template
 
 # Creamos una instancia de la aplicación. __name__ ayuda a Flask a localizar archivos
@@ -7,9 +7,23 @@ app = Flask(__name__)
 # Definimos una ruta. El símbolo @ es un decorador que vincula la URL con la función de abajo
 @app.route("/") # Cuando el usuario entre en la dirección raíz (home)
 def inicio():
-    # Esta función devuelve el código HTML que el navegador mostrará al usuario
-    return render_template("index.html")
-# Importamos la función para manejar plantillas
+    conexion = sqlite3.connect("../videojuegos.db")
+
+    # 2. Configuramos la conexión para que devuelva diccionarios (más fácil para Jinja2)
+    conexion.row_factory = sqlite3.Row
+    cursor = conexion.cursor()
+
+    # 3. Ejecutamos la consulta SQL
+    cursor.execute("SELECT * FROM Videojuego")
+
+    # 4. Guardamos todos los resultados en una variable
+    datos = cursor.fetchall()
+
+    # 5. Cerramos la conexión
+    conexion.close()
+
+    return render_template("index.html", items=datos)
+
 if __name__ == "__main__":
     # Arrancamos el servidor en modo debug para que se reinicie solo al guardar cambios
     app.run(debug=True)
