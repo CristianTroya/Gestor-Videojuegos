c0d0756ec813e878ac926a5d66d5dcb02f0e74a2
Elio <edeldia384@g.educaand.es>
2026-04-28 08:58:42 +0200

    detalle

--- Flask/app.py
+++ Flask/app.py
@@ -4,10 +4,13 @@ from flask import Flask, render_template
 # Creamos una instancia de la aplicación. __name__ ayuda a Flask a localizar archivos
 app = Flask(__name__)
 
+# Ubicación del archivo database
+db_path = os.path.join(os.path.dirname(__file__), "../videojuegos.db")
+
+
 # Definimos una ruta. El símbolo @ es un decorador que vincula la URL con la función de abajo
 @app.route("/") # Cuando el usuario entre en la dirección raíz (home)
 def inicio():
-    db_path = os.path.join(os.path.dirname(__file__), "../videojuegos.db")
     conexion = sqlite3.connect(db_path)
 
     # 2. Configuramos la conexión para que devuelva diccionarios (más fácil para Jinja2)
@@ -26,9 +29,25 @@ def inicio():
     for item in datos:
         print(item["Nombre"], item["Descripción"], item["Imagen"])
 
-
-
     return render_template("index.html", items=datos)
+
+@app.route("/detalle/<int:id_item>")
+def ver_detalle(id_item):
+    # 1. Abrimos conexión con la base de datos
+    conexion = sqlite3.connect(db_path)
+    conexion.row_factory = sqlite3.Row
+    cursor = conexion.cursor()
+    
+    # 2. Buscamos el elemento que tenga exactamente ese ID
+    # El símbolo ? evita ataques de Inyección SQL
+    cursor.execute("SELECT * FROM Videojuego WHERE id = ?", (id_item,))
+    
+    # 3. fetchone() obtiene solo UN resultado (el que coincide con el ID)
+    item_encontrado = cursor.fetchone()
+    conexion.close()
+    
+    # 4. Enviamos los datos del elemento a una plantilla específica
+    return render_template("detalle.html", item=item_encontrado)
 
 if __name__ == "__main__":
     # Arrancamos el servidor en modo debug para que se reinicie solo al guardar cambios
